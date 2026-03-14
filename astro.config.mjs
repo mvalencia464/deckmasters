2f002ed9c836cf6611c6db52da3669100d07de65
mvalencia464 <mauricio@stokeleads.com>
2026-03-13 21:52:55 -0700

    upgraded from Astro 5 to Astro 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   build: {
     inlineStylesheets: 'auto',
   },
